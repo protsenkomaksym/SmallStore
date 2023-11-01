ffa565a39e02db641733a7ae64c67db97b93dc06
protsenkomaksym <maksim20032@hotmail.com>
2023-10-31 21:19:53 -0300

    Migrations: Initial TotalPrice

---
+++ DataLayer/Migrations/20231101001702_Initial.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fullName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    stock = table.Column<int>(type: "int", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductClient",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    idProduct = table.Column<int>(type: "int", nullable: false),
+                    idClient = table.Column<int>(type: "int", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false),
+                    created = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductClient", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ProductClient_Client",
+                        column: x => x.idClient,
+                        principalTable: "Client",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_ProductClient_Product",
+                        column: x => x.idProduct,
+                        principalTable: "Product",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductClient_idClient",
+                table: "ProductClient",
+                column: "idClient");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductClient_idProduct",
+                table: "ProductClient",
+                column: "idProduct");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductClient");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
--- DataLayer/Models/ProductClient.cs
+++ DataLayer/Models/ProductClient.cs
@@ -13,6 +13,8 @@ public partial class ProductClient
 
     public int Quantity { get; set; }
 
+    public decimal totalPrice { get; set; }
+
     public DateTime Created { get; set; }
 
     public virtual Client IdClientNavigation { get; set; } = null!;
